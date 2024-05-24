d538ebceeadae3e00cbec45dd8577fd1e6c35f20
ldárius <ldrius@WINDOWS-0C2PGA4>
2024-05-24 08:49:40 +0200

    Add project files.

---
+++ KikeletPanzio/Szobak.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KikeletPanzio
+{
+    internal class Szobak
+    {
+        int roomNum;
+        int roomCapacity;
+        int roomPrice;
+
+    }
+}
